75648c278d66344bbf5c84c21c90d1a9a26f4469
Mahendradeokar <deokarmahendra424@gmail.com>
2023-10-11 00:48:09 +0530

    Added logout and them btn in dashboard

--- src/app/dashboard/components/dashboard/Sidebar.tsx
+++ src/app/dashboard/components/dashboard/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import UserNav from "./user-profile";
+import { ThemeToggle } from "@/components/Theme";
 
 // import { Playlist } from "../data/playlists";
 export const playlist = [
@@ -22,7 +24,7 @@ interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {}
 export default function Sidebar({ className }: SidebarProps) {
   return (
     <div className={cn(className)}>
-      <div className="space-y-4 py-6 lg:px-8">
+      <div className="space-y-4 flex flex-col py-6 lg:px-8 h-[100%]">
         {/* <div className=""> */}
         <div className="text-center h-16 flex items-center px-4 text-lg font-semibold tracking-tight">
           <div className="text-center">
@@ -41,24 +43,27 @@ export default function Sidebar({ className }: SidebarProps) {
           </div>
           <span>Ecom Soft</span>
         </div>
-        <div className="space-y-1">
-          <Button variant="secondary" className="w-full justify-start">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="mr-2 h-4 w-4"
-            >
-              <circle cx="12" cy="12" r="10" />
-              <polygon points="10 8 16 12 10 16 10 8" />
-            </svg>
-            Profile
-          </Button>
-          {/* <Button variant="ghost" className="w-full justify-start">
+        <div className="flex flex-col flex-1 justify-center">
+          <div className="space-y-1 flex-1">
+            <Button variant="secondary" className="w-full justify-start">
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="mr-2 h-4 w-4"
+              >
+                <path
+                  d="M7.5 0.875C5.49797 0.875 3.875 2.49797 3.875 4.5C3.875 6.15288 4.98124 7.54738 6.49373 7.98351C5.2997 8.12901 4.27557 8.55134 3.50407 9.31167C2.52216 10.2794 2.02502 11.72 2.02502 13.5999C2.02502 13.8623 2.23769 14.0749 2.50002 14.0749C2.76236 14.0749 2.97502 13.8623 2.97502 13.5999C2.97502 11.8799 3.42786 10.7206 4.17091 9.9883C4.91536 9.25463 6.02674 8.87499 7.49995 8.87499C8.97317 8.87499 10.0846 9.25463 10.8291 9.98831C11.5721 10.7206 12.025 11.8799 12.025 13.5999C12.025 13.8623 12.2376 14.0749 12.5 14.0749C12.7623 14.075 12.975 13.8623 12.975 13.6C12.975 11.72 12.4778 10.2794 11.4959 9.31166C10.7244 8.55135 9.70025 8.12903 8.50625 7.98352C10.0187 7.5474 11.125 6.15289 11.125 4.5C11.125 2.49797 9.50203 0.875 7.5 0.875ZM4.825 4.5C4.825 3.02264 6.02264 1.825 7.5 1.825C8.97736 1.825 10.175 3.02264 10.175 4.5C10.175 5.97736 8.97736 7.175 7.5 7.175C6.02264 7.175 4.825 5.97736 4.825 4.5Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              Profile
+            </Button>
+            {/* <Button variant="ghost" className="w-full justify-start">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -95,6 +100,33 @@ export default function Sidebar({ className }: SidebarProps) {
             </svg>
             Radio
           </Button> */}
+          </div>
+          <div className="flex gap-4">
+            {/* <UserNav /> */}
+            <Button
+              variant="outline"
+              className="w-full justify-start flex-1"
+            >
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                transform="rotate(-90)"
+                className="mr-2 h-4 w-4"
+              >
+                <path
+                  d="M3.5 5.00006C3.22386 5.00006 3 5.22392 3 5.50006L3 11.5001C3 11.7762 3.22386 12.0001 3.5 12.0001L11.5 12.0001C11.7761 12.0001 12 11.7762 12 11.5001L12 5.50006C12 5.22392 11.7761 5.00006 11.5 5.00006L10.25 5.00006C9.97386 5.00006 9.75 4.7762 9.75 4.50006C9.75 4.22392 9.97386 4.00006 10.25 4.00006L11.5 4.00006C12.3284 4.00006 13 4.67163 13 5.50006L13 11.5001C13 12.3285 12.3284 13.0001 11.5 13.0001L3.5 13.0001C2.67157 13.0001 2 12.3285 2 11.5001L2 5.50006C2 4.67163 2.67157 4.00006 3.5 4.00006L4.75 4.00006C5.02614 4.00006 5.25 4.22392 5.25 4.50006C5.25 4.7762 5.02614 5.00006 4.75 5.00006L3.5 5.00006ZM7 1.6364L5.5682 3.0682C5.39246 3.24393 5.10754 3.24393 4.9318 3.0682C4.75607 2.89246 4.75607 2.60754 4.9318 2.4318L7.1818 0.181802C7.26619 0.09741 7.38065 0.049999 7.5 0.049999C7.61935 0.049999 7.73381 0.09741 7.8182 0.181802L10.0682 2.4318C10.2439 2.60754 10.2439 2.89246 10.0682 3.0682C9.89246 3.24393 9.60754 3.24393 9.4318 3.0682L8 1.6364L8 8.5C8 8.77614 7.77614 9 7.5 9C7.22386 9 7 8.77614 7 8.5L7 1.6364Z"
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              Log Out
+            </Button>
+            <ThemeToggle />
+          </div>
         </div>
       </div>
     </div>
