7ffc7b7425550f2f5be6c2f7312a061bcd0c8d5a
dipakmahajan01 <dipakmahajan28082000@gmail.com>
2023-11-13 13:02:00 +0530

    middleware changes done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,70 +1,30 @@
 import { StatusCodes } from 'http-status-codes';
 import { NextRequest, NextResponse } from 'next/server';
-import { getTokenData } from './helper/jwt.helper';
 
 export default async function middleware(request: NextRequest) {
   try {
     const token: any = request.cookies.get('token');
-    if (!token) {
-      throw new Error('unauthorized');
-    }
-    await getTokenData(token.value);
-    // if (isAdminRoute(pathname) && role !== "admin") {
-    //   return NextResponse.redirect(new URL('/api/auth/unauthorized', req.url));
-    // }
-    return NextResponse.next();
-  } catch (error: any) {
-    let response: {
-      error: string;
-      errorCode: string;
-      redirect?: string;
-    } = {
-      error: error.message ?? 'Something went wrong!',
-      errorCode: error.code ?? 'UNKNOWN',
-      redirect: '/login',
-    };
 
-    let status = {
-      status: StatusCodes.INTERNAL_SERVER_ERROR,
-    };
-
-    switch (error.code ?? error.message) {
-      case 'ERR_JWT_EXPIRED':
-        response = {
-          error: 'Token expired',
-          errorCode: 'ERR_JWT_EXPIRED',
-          redirect: '/login',
-        };
-        status = {
-          status: StatusCodes.UNAUTHORIZED,
-        };
-        break;
-
-      case 'unauthorized':
-        response = {
-          error: 'Unauthorized request........',
-          errorCode: 'unauthorized',
-          redirect: '/login',
-        };
-
-        status = {
-          status: StatusCodes.UNAUTHORIZED,
-        };
-
-        break;
-
-      default:
-        break;
-    }
-
-    if (!request.nextUrl.pathname.includes('api')) {
+    if (request.nextUrl.pathname.startsWith('/auth')) {
+      if (token) {
+        return NextResponse.next();
+      }
       return NextResponse.redirect(new URL('/login', request.url));
     }
-    delete response.redirect;
-    return NextResponse.json(response, status);
+
+    // login & register routes
+    if (['/login', '/signup'].includes(request.nextUrl.pathname)) {
+      if (token) {
+        return NextResponse.redirect(new URL('/', request.url));
+      }
+      return NextResponse.next();
+    }
+    return NextResponse.next();
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: StatusCodes.INTERNAL_SERVER_ERROR });
   }
 }
 
 export const config = {
-  matcher: `/((?!login|signUp|api/auth/login|api/auth/signup|_next/static|_next/image|favicon.ico).*)`,
+  matcher: ['/', '/login', '/signup', '/api/:path*'],
 };
