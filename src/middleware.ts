421ab9d3e77828e4e18737e8b98758837133042f
Mahendradeokar <deokarmahendra424@gmail.com>
2024-03-12 01:39:36 +0530

    Revert "Removed middleware for testing"
    This reverts commit d264f8d5cf5b202890bd1b3c613e906fbc8acb5e.

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,17 +6,17 @@ import { TOKEN } from './common/constants';
 export default async function middleware(request: NextRequest) {
   try {
     const token = request.cookies.get(TOKEN)?.value;
-    // // login & register routes
-    // if (['/login', '/signUp', '/'].includes(request.nextUrl.pathname)) {
-    //   if (token) {
-    //     return NextResponse.redirect(new URL('/', request.url));
-    //   }
-    //   return NextResponse.next();
-    // }
+    // login & register routes
+    if (['/login', '/signUp'].includes(request.nextUrl.pathname)) {
+      if (token) {
+        return NextResponse.redirect(new URL('/', request.url));
+      }
+      return NextResponse.next();
+    }
 
-    // if (!token) {
-    //   return NextResponse.redirect(new URL('/login', request.url));
-    // }
+    if (!token) {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
     return NextResponse.next();
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: StatusCodes.INTERNAL_SERVER_ERROR });
